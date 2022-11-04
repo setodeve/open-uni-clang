1b5df492321528c27662636f1312067651860d83
setodeve <keinick11@outlook.com>
2022-11-05 08:28:47 +0900

    ex09

--- ex09.c
+++ ex09.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
+  char initial = 'M' ;
 	int score = 42;
   double height = 164.5;
 
-	printf("Mさんの年齢は%d歳、身長はは%fcmです。\n", score, height);
-  printf("Mさんの年齢は%d歳、身長はは%.1fcmです。\n", score, height);
+	printf("%cさんの年齢は%d歳、身長はは%fcmです。\n", initial, score, height);
+  printf("%cさんの年齢は%d歳、身長はは%.1fcmです。\n", initial, score, height);
 	return 0;
 }
