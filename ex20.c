e2ffd1c347daa5088e3051bfc0a90a392b27079f
setodeve <keinick11@outlook.com>
2022-12-12 15:54:15 +0900

    ex20

---
+++ ex20.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	int i;
+	printf("正の整数を入力してください > ");
+	scanf("%d", &i);
+	if (i % 5 ==0)
+		printf("%dは5で割り切れます。\n", i);
+	else
+		printf("%dは5で割り切れません。\n", i);
+	return 0;
+}
