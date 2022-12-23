675b7c4fdc8dd1a94afda7b9f03cd55f36a43bc8
setodeve <keinick11@outlook.com>
2022-12-23 13:56:10 +0900

    ex43,47

---
+++ ex43.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+	int i = 100;
+
+	while (1)
+	{
+		if (i <= 1) break;
+
+    if (i % 3 != 0 || i % 5 != 0){
+      --i;
+      continue;
+    }
+    printf("%d ", i);
+    --i;
+	}
+	printf("\n");
+	return 0;
+}
